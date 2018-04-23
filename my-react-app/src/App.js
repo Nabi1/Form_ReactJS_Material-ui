99fef7ed7a7136e6c6b3556bbe73f3d3a27d3da9
Thibaut C <thibaut.cointet@gmail.com>
2018-04-23 16:57:04 +0200

    Quete010

---
+++ my-react-app/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SignUp from './SignUp'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">This is L'Odyssée d'Homer</h1>
+        </header>
+        <p className="App-intro">
+         <SignUp/>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
+
