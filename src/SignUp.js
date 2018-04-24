2e44719744b2beb3f63d9054f8847f5182101a7d
Maylis <maylis.doucet@gmail.com>
2018-04-24 14:40:39 +0200

    MD_quete010

---
+++ src/SignUp.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+class SignUp extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { email : 'Tape ton email' };
+    this.updateEmailField= this.updateEmailField.bind(this)
+  }
+
+  updateEmailField(event) {
+    this.setState({email: event.target.value});
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>{this.state.email}</h1>
+        <input type="email" name="email" onChange={this.updateEmailField} />
+      </div>
+    );
+  }
+}
+
+export default SignUp;
