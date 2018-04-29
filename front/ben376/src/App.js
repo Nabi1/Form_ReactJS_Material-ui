6220b79306807c4aa7af032a468c0864380f9191
benhur <seurin.b@live.fr>
2018-04-29 13:34:55 +0200

    quete formulaire 2

--- front/ben376/src/App.js
+++ front/ben376/src/App.js
@@ -15,8 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-<br/><br/><br/><br/>
-        <SignUp email=''/>
+        <SignUp />
       </div>
     );
   }
--- front/ben376/src/SignUp.js
+++ front/ben376/src/SignUp.js
@@ -3,20 +3,64 @@ import React, {Component} from 'react';
 class SignUp extends Component {
   constructor(props){
     super(props);
-    this.state = {value: ''}
-  }
+    this.state = {email: ''},
+    this.state = {name: ''},
+    this.state = {password: ''},
+    this.state = {password2: ''},
+    this.state = {lastname: ''}
 
+}
 
 updateEmailField (event) {
-  this.setState({value: event.target.value});
+  this.setState({email: event.target.value});
+}
+updateEmailField2 (event) {
+  this.setState({password: event.target.value});
+}
+updateEmailField3 (event) {
+  this.setState({name: event.target.value});
+}
+updateEmailField4 (event) {
+  this.setState({lastname: event.target.value});
+}
+updateEmailField5 (event) {
+  this.setState({password2: event.target.value});
+}
+
+handleSubmit(event) {
+  console.log(this.state);
+  alert('Ok!');
 
 }
 
   render() {
     return(
       <div>
-        <h1>Voici l'email tapé: <br/>{this.state.value}</h1>
-        <input type="email" name="email" placeholder='youpi yoh' onChange={this.updateEmailField.bind(this)}/>
+        <h1>{JSON.stringify(this.state,1,1)}</h1>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+           <h2>Sign Up</h2>
+           <div>
+              <label>Email address</label>
+              <input type="email" name="email" placeholder='email' required onChange={this.updateEmailField.bind(this)}/>
+            </div>
+           <div>
+              <label>Password</label>
+              <input type='password' placeholder='password' name='password' required onChange={this.updateEmailField2.bind(this)}/>
+           </div>
+           <div>
+              <label>Password Verification</label>
+              <input type='password' placeholder='password2' name='password2' required onChange={this.updateEmailField5.bind(this)}/>
+           </div>
+           <div>
+              <label>Name</label>
+              <input type='text' placeholder='name' name='name' required onChange={this.updateEmailField3.bind(this)}/>
+           </div>
+           <div>
+              <label>LastName</label>
+              <input type='text' placeholder='lastname' name='lastname' required onChange={this.updateEmailField4.bind(this)}/>
+           </div>
+           <input type="submit" value="Send" />
+        </form>
       </div>
     );
   }
