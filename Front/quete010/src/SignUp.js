bd916621576765251fbfb72a1836eba4a364ebda
rado1811 <36564887+rado1811@users.noreply.github.com>
2018-04-30 13:23:02 +0200

    quete010

---
+++ Front/quete010/src/SignUp.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class SignUp extends Component {
+   
+    constructor(props) {
+        super(props);
+            this.state = { value: ''};
+            this.updateEmailField = this.updateEmailField.bind(this);        
+    }
+   
+    updateEmailField(event) {
+        this.setState({email: event.target.value})
+    }
+   
+    render() { 
+        return (
+            <div><h1> email renseigné: { this.state.email } </h1>
+            
+            <input type="email" name="email" placeholder="entrez un email" onChange={ this.updateEmailField } /></div>  
+        )
+    }
+}
+ 
+export default SignUp;
+
+
+
+
+
