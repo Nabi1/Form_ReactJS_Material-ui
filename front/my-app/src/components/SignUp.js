7524745d5d327030bf33059d42cdc5425d1bc79c
fredchene <profchene@gmail.com>
2018-05-07 19:17:17 +0200

    Quete020

--- front/my-app/src/components/SignUp.js
+++ front/my-app/src/components/SignUp.js
@@ -3,24 +3,43 @@ import React, {Component} from 'react';
 class SignUp extends Component {
     constructor(props){
 		super(props);
-		this.state ={email:"test@test.com"};
+		this.state ={
+            email:"test@test.com",
+            password : "monPassw0rd",
+            passwordbis : "monPassw0rdbis",
+            name : "James",
+            lastname : "Bond"
+        };
+        this.handleSubmit= this.handleSubmit.bind(this);
     }
     
-    SwitchEmail(e){
-        this.setState({email:e.target.value})
+    SwitchEmail(e){this.setState({email:e.target.value})};
+    SwitchPassword(e) {this.setState({password:e.target.value})};
+    SwitchPasswordbis(e) {this.setState({passwordbis:e.target.value})};
+    SwitchName(e) {this.setState({name:e.target.value})};
+    SwitchLastname(e){this.setState({lastname:e.target.value})};
+       
+    handleSubmit(e){
+        e.preventDefault();
+        console.log(JSON.stringify(this.state,1,1));    
     }
 
-
-  render(){
-    return(
-        <div>
-            <h1>{this.state.email}</h1>
-            <input type="email"  name="email" email={this.state.email} onChange={this.SwitchEmail.bind(this)}/> 
-        </div>
-   
-    );
-  }
+    render(){
+        return(
+            <div>
+                <h1>{JSON.stringify(this.state,1,1)}</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="email"  name="email" onChange={this.SwitchEmail.bind(this)}/> 
+                    <input type="password"  name="password"onChange={this.SwitchPassword.bind(this)}/> 
+                    <input type="password"  name="passwordbis" onChange={this.SwitchPasswordbis.bind(this)}/> 
+                    <input type="text"  name="name" onChange={this.SwitchName.bind(this)}/> 
+                    <input type="lastname"  name="lastname" onChange={this.SwitchLastname.bind(this)}/>
+                    <input type="submit" value="Soumettre"/>
+                </form>
+            </div>
+        );
+    }
 }
 
-
 export default SignUp;
+
