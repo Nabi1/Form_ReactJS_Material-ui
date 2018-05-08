a54856ec39db61250ea6bc0bc0410b9c78fdfef5
rado1811 <36564887+rado1811@users.noreply.github.com>
2018-05-08 13:11:49 +0200

    quete030

--- Back/routes/index.js
+++ Back/routes/index.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 router.get('/', (req, res) => res.send('youhou'));
 
-router.post('/signup', (req, res, next) => res.send('I am in POST signup'));
+
 
 module.exports = router;
