e91be663293d7c142612e7591e1f0807c484aaa6
rado1811 <36564887+rado1811@users.noreply.github.com>
2018-05-08 13:09:36 +0200

    quete030 modif signup

--- Back/routes/auth/auth.js
+++ Back/routes/auth/auth.js
@@ -3,4 +3,6 @@ const router = express.Router();
 
 router.get('/auth', (req, res) => res.send('I am in GET auth'))
 
+router.post('/signup', (req, res, next) => res.send('I am in POST signup'));
+
 module.exports = router;
