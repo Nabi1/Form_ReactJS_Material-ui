645b56986d914003a1f50bdd605f53e4428ce390
Thibaut C <thibaut.cointet@gmail.com>
2018-05-11 18:11:22 +0200

    optimisation quete 040

--- back/app.js
+++ back/app.js
@@ -1,18 +1,44 @@
+// Mes modules
 const express = require('express');
 const app = express();
-var mysql = require('mysql');
+const logger = require('morgan');
+let mysql = require('mysql');
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+
+// Init mes Routes
+const auth = require('./routes/auth/auth');
+
+// Path
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Routes
-let auth = require('./routes/auth/auth');
-
-app.get('/', (req, res) => {
-  res.send('Get sur / ook');
-});
-
 app.use('/auth', auth);
 
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+// Error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // Render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// My port 
+app.listen(3000, () =>{
+  console.log('Listening on port 3000')
+})
+
 // Export
 module.exports = app;
--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,22 +1,18 @@
 const express = require('express');
 const router = express.Router();
-var mysql = require('mysql');
 const connection = require('../../helpers/db.js')
 
 
 router.post('/signup', (req, res, next) => {
-
-    let select = `INSERT INTO users (email, password, name, lastname) VALUES 
-    ('${req.body.email}','${req.body.password}','${req.body.name}','${req.body.lastname}')`;
-    connection.query(select, (err, res) => {
+   let select = `INSERT INTO users (email, password, name, lastname) VALUES 
+   ('${req.body.email}', '${req.body.password}', '${req.body.name}', '${req.body.lastname}');`
+    connection.query(select, function(err, rows, fields) {
         if (err) {
             res.status(500).end();
         } else {
-            res.status(200).end();
+            res.send('Bien envoyé en base');
         }
-    })
+    });
 });
-
-// Si ce n'est pas ce qui est demandé merci de me donner des pistes
 
 module.exports = router;
