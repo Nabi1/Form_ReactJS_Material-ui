f3b001efcdd676b13c8a62fc0021e635e4328341
Fanny Gaudin <mauriceleptitpoissonrouge@hotmail.fr>
2018-05-11 12:29:47 +0200

    quete 040 - done

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const connection = require('../../helpers/db.js');
 
 router.post('/signup', function(req, res, next) {
-  res.send('I am in POST signup');
+  let user = {email: req.body.email, password: req.body.password, name: req.body.name, lastname: req.body.lastname};
+  connection.query('INSERT INTO users SET ?', user, function(error, results, fields) {
+    if(error){
+      res.status(500).end();
+    }
+    res.send('Ok done !');
+  });
 });
 
 module.exports = router;
