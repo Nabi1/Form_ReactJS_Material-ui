9d59269001b5ac300d9ddc99190851f8fe6f9ad4
Pierrick <quetille.pierrick@gmail.com>
2018-05-14 12:12:28 +0200

    quetille_pierrick_quete020

--- front/src/SignUp.js
+++ front/src/SignUp.js
@@ -4,19 +4,36 @@ class SignUp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email : 'test@test.com'
+      email: '',
+      password: '',
+      passwordbis:'',
+      name: '',
+      lastname: ''
     };
   }
 
   getChange(event) {
-    this.setState({email: event.target.value});
+    this.setState({[event.target.name]:event.target.value});
+    
+  }
+
+  handleSubmit(event) {
+    console.log(`A name was submitted : ${JSON.stringify(this.state,1,1)}`);    
+    event.preventDefault();
   }
 
   render() {
     return(
       <div>
-        <h1>{this.state.email}</h1>
-        <input type="text" value={this.state.value} onChange={this.getChange.bind(this)} />
+        <h1>{JSON.stringify(this.state,1,1)}</h1>
+        <form onSubmit = {this.handleSubmit.bind(this)}>
+          <input type='text' name='email' placeholder = 'nom@email.com' value={this.state.email} onChange={this.getChange.bind(this)} /><br/>
+          <input type='text' name='password' placeholder = 'monPassw0rd' value={this.state.password} onChange={this.getChange.bind(this)}/><br/>
+          <input type='text' name='passwordbis' placeholder = 'monPassw0rd' value={this.state.passwordbis} onChange={this.getChange.bind(this)}/><br/>
+          <input type='text' name='name' placeholder = 'James' value={this.state.name} onChange={this.getChange.bind(this)}/><br/>
+          <input type='text' name='lastname' placeholder = 'Bond' value={this.state.lastname} onChange={this.getChange.bind(this)}/><br/>
+          <input type='submit' value='Soumettre'/>
+        </form>
       </div>
     );
   }
