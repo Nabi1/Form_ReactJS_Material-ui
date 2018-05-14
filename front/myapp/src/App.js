f0b441e5e79ff0caf7dbd505ea95c1e625312613
Fanny Gaudin <mauriceleptitpoissonrouge@hotmail.fr>
2018-05-14 16:31:06 +0200

    quetes060 - done

--- front/myapp/src/App.js
+++ front/myapp/src/App.js
@@ -2,17 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SignUp from './SignUp.js';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 // import MyTimer from './MyTimer.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <SignUp />
+        <MuiThemeProvider>
+          <SignUp />
+        </MuiThemeProvider>
         {/* <MyTimer /> */}
       </div>
     );
--- front/myapp/src/SignUp.js
+++ front/myapp/src/SignUp.js
@@ -1,4 +1,18 @@
 import React, { Component } from 'react';
+import Grid from 'material-ui/Grid';
+import Paper from 'material-ui/Paper';
+import Typography from 'material-ui/Typography';
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
+import Snackbar from 'material-ui/Snackbar';
+
+const styles = theme => ({
+  root: theme.mixins.gutters({
+    paddingTop: 16,
+    paddingBottom: 16,
+    marginTop: theme.spacing.unit * 3,
+  }),
+});
 
 class SignUp extends Component{
   constructor(props){
@@ -38,25 +52,83 @@ class SignUp extends Component{
         err  =>  this.setState({"flash":  err.flash})
     )
   }
+  handleClick = state => () => {
+   this.setState({ open: true, ...state });
+  };
+  handleClose = () => {
+    this.setState({ open: false });
+  };
   render(){
+    const { vertical, horizontal, open } = this.state;
     return(
-      <div>
-        <h1>{JSON.stringify(this.state,1,1)}</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label>Email </label>
-          <input type="email" name="email" onChange={this.handleChange}/>
-          <label>Password </label>
-          <input type="password" name="password" onChange={this.handleChange}/>
-          <label>Password again </label>
-          <input type="password" name="passwordCheck" onChange={this.handleChange}/>
-          <label>Name </label>
-          <input type="text" name="name" onChange={this.handleChange}/>
-          <label>Lastname </label>
-          <input type="text" name="lastname" onChange={this.handleChange}/>
-          <input type="submit" value="Soumettre"/>
-        </form>
-        <p>{this.state.flash}</p>
-    </div>
+      <Paper elevation={4}>
+      <Grid container spacing={16}>
+        <Grid  item  xs={12}  sm={6}>
+          <img alt="C'est Homer !" src="http://images.innoveduc.fr/react_odyssey_homer/wildhomer.png"  />
+        </Grid>
+        <Grid  item  xs={12}  sm={6}>
+          <div>
+            <Typography variant="headline" component="h3">Sign up!</Typography>
+            <form onSubmit={this.handleSubmit}>
+              <TextField
+                id="email"
+                name="email"
+                label="Email"
+                onChange={this.handleChange}
+                fullWidth
+                margin="normal"
+              />
+              <TextField
+                id="password"
+                name="password"
+                label="Password"
+                type="password"
+                onChange={this.handleChange}
+                fullWidth
+                margin="normal"
+              />
+              <TextField
+                id="passwordCheck"
+                name="passwordCheck"
+                label="Password"
+                type="password"
+                onChange={this.handleChange}
+                fullWidth
+                margin="normal"
+              />
+              <TextField
+                id="name"
+                name="name"
+                label="Name"
+                onChange={this.handleChange}
+                fullWidth
+                margin="normal"
+              />
+              <TextField
+                id="lastname"
+                name="lastname"
+                label="Lastname"
+                onChange={this.handleChange}
+                fullWidth
+                margin="normal"
+              />
+              <Button onClick={this.handleClick({ vertical: 'bottom', horizontal: 'center' })} type="submit" variant="raised" color="primary">
+                Submit
+              </Button>
+            </form>
+            <Snackbar
+              anchorOrigin={{ vertical, horizontal }}
+              open={open}
+              onClose={this.handleClose}
+              ContentProps={{
+                'aria-describedby': 'message-id',
+              }}
+              message={<span id="message-id">{this.state.flash}</span>}
+            />
+        </div>
+        </Grid>
+      </Grid>
+    </Paper>
     )
   }
 }
