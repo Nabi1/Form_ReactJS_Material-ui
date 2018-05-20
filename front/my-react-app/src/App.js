d6a4502d7bbb9561eed55b810bfbc7a8242d3272
Thibaut C <thibaut.cointet@gmail.com>
2018-05-20 18:57:02 +0200

    pimp btn

--- front/my-react-app/src/App.js
+++ front/my-react-app/src/App.js
@@ -1,22 +1,40 @@
 // React
-import React, { Component } from 'react';
-import {MuiThemeProvider, Paper ,Grid, createMuiTheme} from 'material-ui';
+import React, { Component } from "react";
+import { MuiThemeProvider, Paper, Grid, createMuiTheme } from "material-ui";
 // Components
-import SignUp from './Components/SignUp'
+import SignUp from "./Components/SignUp";
 // CSS
-import 'typeface-roboto'
-import './App.css';
-import logo from './logo.svg';
+import "typeface-roboto";
+import "./App.css";
+import logo from "./logo.svg";
 
-const theme = createMuiTheme();
+const theme = createMuiTheme({
+  overrides: {
+    // Name of the component ⚛️ / style shee
+    MuiButton: {
+      // Name of the rule
+      root: {
+        // Some CSS
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .30)',
+      },
+    },
+  },
+});
+
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      open:false
-    }
+      open: false
+    };
   }
- 
+
   render() {
     return (
       <div className="App">
@@ -27,16 +45,26 @@ class App extends Component {
         </header>
         <h1>Welcome</h1>
         <MuiThemeProvider theme={theme}>
-          <Paper elevation={12} style={{ margin:  32 }}>
+          <Paper elevation={12} style={{ margin: 32 }}>
             <Grid container spacing={24} align="center">
-              <Grid  item  xs={12} sm={6} container alignItems='center' style={{ height:  '100%' }}>
-                <Grid container alignContent='center'  >
-                  <Grid style={{ 'textAlign':  'center' }} align="center">
-                    <img src="http://images.innoveduc.fr/react_odyssey_homer/wildhomer.png" alt='Homer' />
+              <Grid
+                item
+                xs={12}
+                sm={6}
+                container
+                alignItems="center"
+                style={{ height: "100%" }}
+              >
+                <Grid container alignContent="center">
+                  <Grid style={{ textAlign: "center" }} align="center">
+                    <img
+                      src="http://images.innoveduc.fr/react_odyssey_homer/wildhomer.png"
+                      alt="Homer"
+                    />
                   </Grid>
                 </Grid>
               </Grid>
-              <Grid item  xs={12}  sm={6} >
+              <Grid item xs={12} sm={6}>
                 <SignUp />
               </Grid>
             </Grid>
@@ -48,4 +76,3 @@ class App extends Component {
 }
 
 export default App;
-
--- front/my-react-app/src/Components/AlertDialogSlide.jsx
+++ front/my-react-app/src/Components/AlertDialogSlide.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Slide from '@material-ui/core/Slide';
+// React
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Slide from "@material-ui/core/Slide";
 
 function Transition(props) {
   return <Slide direction="up" {...props} />;
 }
 
 class AlertDialogSlide extends React.Component {
-
   render() {
     return (
       <div>
@@ -25,18 +25,16 @@ class AlertDialogSlide extends React.Component {
           aria-describedby="alert-dialog-slide-description"
         >
           <DialogTitle id="alert-dialog-slide-title">
-            {'Everything is NOT fine'}
+            {"Everything is NOT fine"}
           </DialogTitle>
           <DialogContent>
-          {
-            this.props.messageDialogue.map((message,i) => {
+            {this.props.messageDialogue.map((message, i) => {
               return (
                 <DialogContentText key={i} id="alert-dialog-slide-description">
                   {message}
-                </DialogContentText> 
-              ) 
-            })          
-          }
+                </DialogContentText>
+              );
+            })}
           </DialogContent>
           <DialogActions>
             <Button onClick={this.props.hideDialogueBox} color="primary">
